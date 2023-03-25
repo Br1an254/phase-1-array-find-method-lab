84dc5b600cc18c5968d1e5c109308f4b0b058b98
Brian Babu <briansmkenya@gmail.com>
2023-03-25 19:06:31 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superBowlWin(arr) {
+    if(arr.result === "W") {
+        return arr.year
+    }
+}
+
+ record.find(superBowlWin);
+
+ console.log(superBowlWin(...record))
